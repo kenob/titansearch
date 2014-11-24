6d601f7496680a49bd4bdc6192edc035e1d3cca0
kenoob <keno.ob@live.com>
2014-11-24 04:23:05 -0500

    indexing script

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-wiki_on_the_internwt
+wiki_on_the_internet
 ====================
 
 Stuff from internet relevant to a Wiki article. (Stuff : News, Tweets...)
--- run.py
+++ run.py
@@ -1,11 +1,30 @@
 from flask.ext.script import Manager
 from app import application
+import os
+from urllib import *
+import json
+import time
+import requests
 
 manager = Manager(application)
 
 @manager.command
 def runserver():
 	application.run(debug = True)
+
+@manager.command
+def refresh_index(corpus_type="NYC"):
+	base_url = application.config.get('SOLR_NEWSCOLLECTION')
+	r = requests.get('%s/dataimport?command=full-import' % base_url)
+	while True:
+		status_conn = urlopen('%s/dataimport?command=status&wt=python' % base_url)
+		status_response = eval(status_conn.read())
+		if status_response['status'] is 'idle':
+			print status_response["statusMessages"][""]
+			break
+		time.sleep(10)
+		print "processed %s documents, still working..." % status_response["statusMessages"]["Total Documents Processed"]
+
 
 if __name__=="__main__":
 	manager.run()
