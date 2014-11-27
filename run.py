40ace0340e69f1fe78d6c7a9a711439f0713a044
kenob <oobrutu@buffalo.edu>
2014-11-26 22:48:26 -0500

    indexing script fixes

--- run.py
+++ run.py
@@ -23,18 +23,23 @@ def refresh_index(corpus_type="NYC"):
 	except:
 		print "Import could not be started. Please make sure the newsArticleCollection instance is configured properly and running"
 		return
-
+	retries = 0
 	while True:
-		print("."),
 		try:
 			status_conn = urlopen('%s/dataimport?command=status&wt=python' % base_url)
 			status_response = eval(status_conn.read())
+			stat = status_response["statusMessages"].get("Total Documents Processed",False)
+			if stat:
+				print "... %s documents processed" % stat			
 			if status_response['status'] is 'idle':
 				print "\n", status_response["statusMessages"][""]
 				break
 			time.sleep(5)
 		except:
-			pass
+			retries = retries + 1
+			if retries > 10:
+				print "Cannot Get status, please check your solr server"
+				break
 
 
 @manager.command
