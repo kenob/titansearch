bba3765a059f88f3c1601cee83093e421c8e78c8
kenob <oobrutu@buffalo.edu>
2014-11-26 19:11:48 -0500

    error handling for kwextractor

--- app/keyword_extractor.py
+++ app/keyword_extractor.py
@@ -2,7 +2,7 @@
 import urllib
 import json
 
-body_text = '''The Hubble Space Telescope (HST) is a space telescope that was launched into low Earth orbit in 1990 and remains in operation. With a 2.4-meter (7.9 ft) mirror, Hubble's four main instruments observe in the near ultraviolet, visible, and near infrared spectra. The telescope is named after the astronomer Edwin Hubble.
+text = '''The Hubble Space Telescope (HST) is a space telescope that was launched into low Earth orbit in 1990 and remains in operation. With a 2.4-meter (7.9 ft) mirror, Hubble's four main instruments observe in the near ultraviolet, visible, and near infrared spectra. The telescope is named after the astronomer Edwin Hubble.
 
 Hubble's orbit outside the distortion of Earth's atmosphere allows it to take extremely high-resolution images with almost no background light. Hubble has recorded some of the most detailed visible-light images ever, allowing a deep view into space and time. Many Hubble observations have led to breakthroughs in astrophysics, such as accurately determining the rate of expansion of the universe.
 
@@ -13,14 +13,18 @@ Space telescopes were proposed as early as 1923. Hubble was funded in the 1970s,
 Hubble is the only telescope designed to be serviced in space by astronauts. After launch by Space Shuttle Discovery in 1990, four subsequent Space Shuttle missions repaired, upgraded, and replaced systems on the telescope. A fifth mission was canceled on safety grounds following the Columbia disaster. However, after spirited public discussion, NASA administrator Mike Griffin approved one final servicing mission, completed in 2009. The telescope is still operating as of 2014, and may last until 2020.[6] Its scientific successor, the James Webb Space Telescope (JWST), is scheduled for launch in 2018.'''
 
 
-precision = 0.9;
-app_key = '9be64884189dc6bfb19e341ee93a48b7'
-app_id = 'af1ed7e7'
-params = urllib.urlencode({'lang': 'en', 'text': body_text, 'min_confidence' : precision,  'include' :'types,abstract,categories,lod','$app_id': app_id, '$app_key' : app_key   })
-response = urllib.urlopen("https://api.dandelion.eu/datatxt/nex/v1/?%s" % params)
+def extract_keywords(body_text=text):
+	precision = 0.9;
+	app_key = '9be64884189dc6bfb19e341ee93a48b7'
+	app_id = 'af1ed7e7'
+	params = urllib.urlencode({'lang': 'en', 'text': body_text, 'min_confidence' : precision,  'include' :'types,abstract,categories,lod','$app_id': app_id, '$app_key' : app_key   })
+	
+	try:
+		response = urllib.urlopen("https://api.dandelion.eu/datatxt/nex/v1/?%s" % params)
+		data = json.load(response)
+	except:
+		return dict(status=False, keywords=[])
 
-data = json.load(response)
-
-for datum in data['annotations']:
-	print datum['spot']
+	return dict(status=True, keywords=[datum['spot'] for datum in data['annotations']])
+	
 
--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from urllib import *
 import json
 import time
 import requests
+from app import keyword_extractor
 
 manager = Manager(application)
 
@@ -15,15 +16,35 @@ def runserver():
 @manager.command
 def refresh_index(corpus_type="NYC"):
 	base_url = application.config.get('SOLR_NEWSCOLLECTION')
-	r = requests.get('%s/dataimport?command=full-import' % base_url)
+	try:
+		r = requests.get('%s/dataimport?command=full-import' % base_url)
+		print "Started indexing"
+	except:
+		print "Import could not be started. Please make sure the newsArticleCollection instance is configured properly and running"
+		return
+
 	while True:
-		status_conn = urlopen('%s/dataimport?command=status&wt=python' % base_url)
-		status_response = eval(status_conn.read())
-		if status_response['status'] is 'idle':
-			print status_response["statusMessages"][""]
-			break
-		time.sleep(10)
-		print "still working..."
+		print("."),
+		try:
+			status_conn = urlopen('%s/dataimport?command=status&wt=python' % base_url)
+			status_response = eval(status_conn.read())
+			if status_response['status'] is 'idle':
+				print "\n", status_response["statusMessages"][""]
+				break
+			time.sleep(5)
+		except:
+			pass
+
+
+@manager.command
+def test_kwextractor():
+	res = keyword_extractor.extract_keywords()
+	if res['status']:
+		for k in res['keywords']:
+			print k
+	else:
+		print "There was a connection problem"
+
 
 
 if __name__=="__main__":
